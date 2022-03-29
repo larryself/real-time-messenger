a773a3178c7f74c77e0d253c1aed45d5eb7eb5a5
Yan Musikhin <yanmusikhin@gmail.com>
2022-03-29 15:25:13 +0400

    edit auth module

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -4,18 +4,23 @@ import { UsersModule } from '../users/users.module';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './strategy/jwt.strategy';
-import { jwtConstants } from './constants/constants';
 import { AuthController } from './auth.controller';
 import { JwtRefreshStrategy } from './strategy/jwt-refresh.strategy';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   controllers: [AuthController],
   imports: [
     forwardRef(() => UsersModule),
     PassportModule,
-    JwtModule.register({
-      secret: jwtConstants.secret,
-      signOptions: { expiresIn: 36666 },
+    ConfigModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: config.get<string>('JWT_EXPIRES_IN') },
+      }),
     }),
   ],
   providers: [AuthService, JwtStrategy, JwtRefreshStrategy],
