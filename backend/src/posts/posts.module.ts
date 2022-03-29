5e2dcecacc500ca937720d033868401713f43f7d
Yan Musikhin <yanmusikhin@gmail.com>
2022-03-29 16:05:52 +0400

    add AuthModule in imports

--- backend/src/posts/posts.module.ts
+++ backend/src/posts/posts.module.ts
@@ -3,9 +3,10 @@ import { PostService } from './services/posts.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostEntity } from './entities/post.entity';
 import { PostsController } from './posts.controller';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PostEntity])],
+  imports: [TypeOrmModule.forFeature([PostEntity]), AuthModule],
   providers: [PostService],
   controllers: [PostsController],
   exports: [PostService],
