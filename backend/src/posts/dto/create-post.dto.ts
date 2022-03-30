bca47e117fd556925ec2266fc535cd0f0c1bc36e
Yan Musikhin <yanmusikhin@gmail.com>
2022-03-30 13:27:17 +0400

    small fix

--- backend/src/posts/dto/create-post.dto.ts
+++ backend/src/posts/dto/create-post.dto.ts
@@ -1,12 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreatePostDto {
-  // @ApiProperty({
-  //   example: 'текст сообщения',
-  //   description: 'текст сообщения',
-  // })
-  // readonly from?: string;
-
   @ApiProperty({
     example: 'Автор сообщения',
     description: 'Автор сообщения',
--- backend/src/posts/dto/response-post.dto.ts
+++ backend/src/posts/dto/response-post.dto.ts
@@ -1,5 +1,24 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class MessageResponseDTO {
+  @ApiProperty({
+    example: 'id сообщения',
+    description: 'id сообщения',
+    type: Number,
+  })
   id: number;
+
+  @ApiProperty({
+    example: 'текст сообщения',
+    description: 'текст сообщения',
+    type: String,
+  })
   message: string;
+
+  @ApiProperty({
+    example: 'Автор сообщения',
+    description: 'Автор сообщения',
+    type: String,
+  })
   to: string;
 }
